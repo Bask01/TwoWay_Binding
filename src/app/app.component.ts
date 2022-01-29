80fd85483774302e346a0287552331c5b6448609
bask01 <bask@sheridancollege.ca>
2022-01-29 08:55:20 -0500

    Class imported

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import { MyInfo } from './myClasses/myInfo';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
+
   title = 'A3bask';
+
+  bio: MyInfo = {
+  id : 991618972,
+  name : "Kubra Bas",
+  username : "bask",
+  program : "CP",
+  email : "bask@sheridancollege.ca",
+  homecountry : "Canada",
+  picturename : "girl.jpg"
+  
+  }
 }
